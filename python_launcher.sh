8670aab13ca4d8567c9530a7f4a5dee3cefb9fc5
Sankar Biswas <sankarb475@gmail.com>
2018-12-13 14:07:55 +0530

    Update python_launcher.sh

--- python_launcher.sh
+++ python_launcher.sh
@@ -1,3 +1,3 @@
-echo -e "Starting the Hermes Processing Engine Launcher!!"
+echo -e "Starting the Application Processing Engine Launcher!!"
 nohup python ${APP_HOME}/conf/SparkLauncher.py > /dev/null 2>&1 &
 echo $! > SparkLauncher_run.pid
